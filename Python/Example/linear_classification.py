a6dd8f9e0ef69f4f4ec50ed12f41790531e3ea0a
Daniel Salazar <dsalazar10@horizon.csueastbay.edu>
2018-01-28 14:26:21 -0800

    Update linear_classification.py

--- Python/Example/linear_classification.py
+++ Python/Example/linear_classification.py
@@ -3,6 +3,18 @@
 """
 Created on Sun Jan 28 10:51:13 2018
 
+
+An intro to linear classification using a Linear Support Vector Machine (SVM) 
+which constructs a maximum-margin separating hyperplane between data classes 
+in an n-dimensional space.
+
+This will work well for practical problems such as document classification, 
+and more generally for problems with many variables (features), reaching 
+accuracy levels comparable to non-linear classifiers while taking less time to 
+train and use. In Y. Yang, X. Liu, "A re-examination of text categorization", 
+Proc. ACM SIGIR Conference, pp. 42–49, (1999)., it is stated that SVM was
+introduced for solving two-class pattern recognition problems.
+
 https://www.pyimagesearch.com/2016/08/22/an-intro-to-linear-classification-with-python/
 
 parameterization is the process of defining the necessary parameters of a 
@@ -53,12 +65,6 @@ There are two primary advantages to utilizing parameterized learning:
     adding in the bias b. Doing this is substantially faster than needing 
     to compare each testing point to every training example.
 
-
-An intro to linear classification using a Linear Support Vector Machine (SVM) 
-which constructs a maximum-margin separating hyperplane between data classes 
-in an n-dimensional space.
-
-@author: xeno
 """
 
 # import the necessary packages
@@ -67,33 +73,27 @@ from sklearn.svm import LinearSVC
 from sklearn.metrics import classification_report
 from sklearn.cross_validation import train_test_split
 from imutils import paths
-import numpy as np
-import argparse
-import imutils
-import cv2
-import os
+import numpy as np # array
+import argparse # ArgumentParser class
+import cv2 # cvtColor, calcHist, normalize, imread
+import os # path.sep
 
 # Accepts an input image, converts it to the HSV color space, and then
 # computes a 3D color histogram using the supplied number of bins for each
 # channel.
 def extract_color_histogram(image, bins=(8, 8, 8)):
-	# extract a 3D color histogram from the HSV color space using
-	# the supplied number of `bins` per channel
-	hsv = cv2.cvtColor(image, cv2.COLOR_BGR2HSV)
-	hist = cv2.calcHist([hsv], [0, 1, 2], None, bins,
-		[0, 180, 0, 256, 0, 256])
-	# handle normalizing the histogram if we are using OpenCV 2
-	if imutils.is_cv2():
-		hist = cv2.normalize(hist)	
-	else: # otherwise, perform "in place" normalization in OpenCV 3
-		cv2.normalize(hist, hist)
-	# return the flattened histogram as the feature vector
-	return hist.flatten()
+    # extract a 3D color histogram from the HSV color space using
+    # the supplied number of `bins` per channel
+    hsv = cv2.cvtColor(image, cv2.COLOR_BGR2HSV)
+    hist = cv2.calcHist([hsv], [0, 1, 2], None, bins,[0, 180, 0, 256, 0, 256])
+    #handle normalizing the histogram
+    hist = cv2.normalize(hist)
+    # return the flattened histogram as the feature vector
+    return hist.flatten()
 
-# construct the argument parse and parse the arguments
+# Parse the path to our input Kaggle Dogs vs. Cats dataset
 ap = argparse.ArgumentParser()
-ap.add_argument("-d", "--dataset", required=True,
-	help="path to input dataset")
+ap.add_argument("-d", "--dataset", required=True,help="path to input dataset")
 args = vars(ap.parse_args())
  
 # grab the list of images that we'll be describing
@@ -104,24 +104,23 @@ imagePaths = list(paths.list_images(args["dataset"]))
 data = []
 labels = []
 
-# loop over the input images
+# loop over the input imagePaths
 for (i, imagePath) in enumerate(imagePaths):
 	# load the image and extract the class label (assuming that our
 	# path as the format: /path/to/dataset/{class}.{image_num}.jpg
 	image = cv2.imread(imagePath)
 	label = imagePath.split(os.path.sep)[-1].split(".")[0]
- 
 	# extract a color histogram from the image, then update the
 	# data matrix and labels list
 	hist = extract_color_histogram(image)
 	data.append(hist)
 	labels.append(label)
- 
 	# show an update every 1,000 images
 	if i > 0 and i % 1000 == 0:
 		print("[INFO] processed {}/{}".format(i, len(imagePaths)))
         
-# encode the labels, converting them from strings to integers
+# The labels  list is represented as a list of strings, either “dog” or “cat”
+# So we encode the labels, converting them from strings to integers (1 or 0)
 le = LabelEncoder()
 labels = le.fit_transform(labels)
 
@@ -141,4 +140,3 @@ print("[INFO] evaluating classifier...")
 predictions = model.predict(testData)
 print(classification_report(testLabels, predictions,
 	target_names=le.classes_))
-
